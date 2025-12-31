5b719896438b560821e23b1eda96959e3078afc9
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-31 20:33:50 +0000

    Basic UI created

--- WhoGetsASticker_Challenge/ContentView.swift
+++ WhoGetsASticker_Challenge/ContentView.swift
@@ -9,12 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Who Gets a Sticker?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            HStack {
+                Button("Clear") {
+                    
+                }
+                .tint(.red)
+                Spacer()
+                Button("Pick a Winner") {
+                    
+                }
+                .tint(.green)
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Image("sticker")
+                .resizable()
+                .scaledToFit()
+            
+            Spacer()
+            
+            Text("This Week's Psters Are:")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Text("No Posters This Week")
+                .font(.largeTitle)
+            
+            HStack {
+                TextField("enter name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
+            
         }
         .padding()
     }
